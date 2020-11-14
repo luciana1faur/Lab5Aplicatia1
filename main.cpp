45c83749f0bfa8a7f5734b33f4df427cdfa546df
Faur Luciana <luciana_faur@yahoo.com>
2020-11-14 07:47:42 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int g;
+  double h;
+  g = 83;
+  h = 1.78;
+  double imc;
+  imc = g/(h*h);
+  cout << "Indicele de masa corporala IMC este " << imc << endl;
+}
